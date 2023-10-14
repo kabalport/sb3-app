90ea0e4c780b3334d8c097979207fc6b7f8e630c
kabalport <kabalport@gmail.com>
2023-10-14 15:52:58 +0900

    integrationTest mysql

--- src/test/java/com/kabalport/sb3app/Sb3appApplicationTests.java
+++ src/test/java/com/kabalport/sb3app/Sb3appApplicationTests.java
@@ -1,13 +1,36 @@
 package com.kabalport.sb3app;
 
+import com.kabalport.sb3app.student.model.StudentScoreFixture;
+import com.kabalport.sb3app.student.repository.StudentScoreRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootTest
-class Sb3appApplicationTests {
+
+class Sb3appApplicationTests extends IntegrationTest {
+	@Autowired
+	private StudentScoreRepository studentScoreRepository;
+
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Test
 	void contextLoads() {
+		var studentScore = StudentScoreFixture.passed();
+		var savedStudentScore = studentScoreRepository.save(studentScore);
+
+		entityManager.flush();
+		entityManager.clear();
+
+		var queryStudentScore = studentScoreRepository.findById(savedStudentScore.getId()).orElseThrow();
+
+		Assertions.assertEquals(savedStudentScore.getId(), queryStudentScore.getId());
+		Assertions.assertEquals(savedStudentScore.getStudentName(), queryStudentScore.getStudentName());
+		Assertions.assertEquals(savedStudentScore.getMathScore(), queryStudentScore.getMathScore());
+		Assertions.assertEquals(savedStudentScore.getEnglishScore(), queryStudentScore.getEnglishScore());
+		Assertions.assertEquals(savedStudentScore.getKorScore(), queryStudentScore.getKorScore());
 	}
 
 }
