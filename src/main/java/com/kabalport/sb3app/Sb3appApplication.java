1eb69eb06a54ec0acb1bdade5132458579ceba3e
kabalport <kabalport@gmail.com>
2023-10-15 17:37:56 +0900

    git pre commit test2

--- src/main/java/com/kabalport/sb3app/Sb3appApplication.java
+++ src/main/java/com/kabalport/sb3app/Sb3appApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
+
+
+
+
 @SpringBootApplication
 public class Sb3appApplication {
 
